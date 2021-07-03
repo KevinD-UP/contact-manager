756bf4be29fb93b609871f0c7f0d846b97e89353
Kévin DANG <kevin.dang01@etu.univ-paris-diderot.fr>
2021-07-04 00:00:30 +0200

    projet complet

---
+++ frontend/src/app/contact/contact.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Contact } from '../contact';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  displayedColumns : string[] = ['id', 'name', 'title', 'email', 'phone', 'address', 'city', 'actions'];
+  dataSource: any = [];
+  contact: any = {};
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.readContacts().subscribe((result) => {   
+      this.dataSource = result;
+    })
+  }
+
+  selectContact(contact: Contact){
+    this.contact = contact;
+  }
+
+  newContact(){
+    this.contact = {};
+  }
+
+  createContact(f: any){
+    this.apiService.createContact(f.value).subscribe((result)=>{
+      console.log(result);
+    });
+    
+  }
+
+  deleteContact(id: number){
+    this.apiService.deleteContact(id).subscribe((result)=>{
+      console.log(result);
+    });
+  }
+
+  updateContact(f: any){
+    f.value.id = this.contact['id'];
+    this.apiService.updateContact(f.value).subscribe((result)=>{
+      console.log(result);
+    });
+  }
+
+}
